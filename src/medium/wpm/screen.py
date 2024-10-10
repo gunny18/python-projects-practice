da6619d764b426119ee309dafb9ceca418ab2898
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2024-10-10 15:44:00 +0530

    Added comments on curses usage

--- src/medium/wpm/screen.py
+++ src/medium/wpm/screen.py
@@ -4,15 +4,24 @@ import time
 
 
 def start_screen(stdscr):
+    # clears all text from screen
     stdscr.clear()
+
+    # adds the text to screen at pos 0,0 by default
     stdscr.addstr("Welcome to the speed typing test!")
     stdscr.addstr("\nPress any key to begin")
+
+    # refresh screen to apply the changes
     stdscr.refresh()
+
+    # waiting for a key press event - blocking code!!
     stdscr.getkey()
 
 
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+
+    # first val is to move vertically, second horizontally on screen
     stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
@@ -21,12 +30,16 @@ def display_text(stdscr, target, current, wpm=0):
             color = curses.color_pair(1)
         else:
             color = curses.color_pair(2)
+        # can use the color pair numbers to apply a color combo/pair
         stdscr.addstr(0, i, char, color)
 
 
 def wpm_screen(stdscr):
     target = "This is a simple wpm test!"
     current_text = []
+
+    # to ensure, the key event is not blocking.
+    # to make sure the wpm will decrease if we are not typing anything, instead of being static and waiting for a keypress
     stdscr.nodelay(True)
 
     start = time.time()
@@ -37,19 +50,23 @@ def wpm_screen(stdscr):
         display_text(stdscr, target, current_text, wpm)
         stdscr.refresh()
 
+        # check to target and what we typed is matching, then exit
         if target == "".join(current_text):
             stdscr.nodelay(False)
             break
 
+        # in the no delay mode of screen, waiting for keypress will give an error, hence needs to be wrapped in try except
         try:
             key = stdscr.getkey()
         except:
             continue
 
+        # checks if the esc key is pressed. 27 is the ASCII code of esc key
         if ord(key) == 27:
             stdscr.nodelay(False)
             break
 
+        # checking if backspace is pressed. We need to remove the char we typed if that happens, instead of default behavior of curses backspace event
         if key in ("KEY_BACKSPACE", "\b", "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
@@ -57,7 +74,13 @@ def wpm_screen(stdscr):
             current_text.append(key)
 
 
+# the wrapper passes a stdscr (screen) variable, which will be used to control the operations on the screen
 def main(stdscr):
+
+    # to customize the screen colors
+    # 1st arg is a number to identify the color combo
+    # second arg is the font color of text
+    # third arg is for background color of text
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -73,4 +96,5 @@ def main(stdscr):
             break
 
 
+# must be done to use curses lib
 wrapper(main)
